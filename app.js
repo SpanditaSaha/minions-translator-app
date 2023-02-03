505f4e145d97cc45c97107f4683805f2ce2cf52b
Spandita Saha <spandita406@gmail.com>
2023-02-03 17:44:22 +0530

    upto input

---
+++ app.js
@@ -0,0 +1,19 @@
+/* alert, prompt 
+console.log("Script is working from a different file.");
+var username=prompt("Give me your username");
+alert("This script works! "+username);*/
+
+/* 1. for textarea:  document.querySelector("textarea")
+2. for class btn-primary:  document.querySelector(".btn-primary")
+3. for id input-btn: document.querySelector("#input-btn")
+4. for attributr name="translator": document.querySelector("input[name='translator']")*/
+
+var btnTranslate=document.querySelector("#btn-translate")
+var txtInput=document.querySelector("#txt-input")
+
+function clickHandler()
+{
+    console.log("Clicked!")
+    console.log("input",txtInput.value)
+}
+btnTranslate.addEventListener("click", clickHandler)
