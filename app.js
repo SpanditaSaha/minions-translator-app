4f8168ae2f21d8dd3afd1f0fd727ccafe5de5292
Spandita Saha <spandita406@gmail.com>
2023-02-05 00:50:47 +0530

    video completed code

--- app.js
+++ app.js
@@ -12,16 +12,33 @@ var btnTranslate=document.querySelector("#btn-translate")
 var txtInput=document.querySelector("#txt-input")
 var outputDiv=document.querySelector("#output")
 
+//var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"//
+var serverUrl="	https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationUrl(text){
+    return serverUrl+"?text="+text;
+}
 /*function clickHandler()
 {
     console.log("Clicked!")
     console.log("input",txtInput.value)
 }
 btnTranslate.addEventListener("click", clickHandler)*/
+//for error handling//
+function errorHandler(error)
+{
+    console.log("Error message",error);
+    alert("something wrong with server! Try again after sometime.");
+}
 // Input - Output Processing //
 function clickHandler()
 {
-    outputDiv.innerText="Yes you are "+ txtInput.value ;
+    //outputDiv.innerText="Yes you are "+ txtInput.value ;
+    fetch(getTranslationUrl(txtInput.value)).then(response=>response.json()).then(json=>
+        {
+            var translatedText=json.contents.translated;
+            outputDiv.innerText=translatedText;})
+            .catch(errorHandler)
+};
 
-}
 btnTranslate.addEventListener("click",clickHandler);
